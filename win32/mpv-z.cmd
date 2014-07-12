341b28e7d84692ffd15c36475fbafe71d5118af0
coreb1te <coreb1te@yahoo.co.uk>
2014-07-12 11:19:28 +0200

    Add input (%1) parameter

--- win32/mpv-z.cmd
+++ win32/mpv-z.cmd
@@ -3,13 +3,13 @@
 rem # Small zenity GUI for mpv on Windows.
 rem # Created by coreb1te (coreb1te@yahoo.co.uk)
 rem # Licensed by GPL v.2
-rem # Last update: 10-07-2014
+rem # Last update: 12-07-2014
 rem # --------------------------------------
 rem # zenity for windows from: 
 rem #    https://osspack32.googlecode.com/files/zenity.exe
 
- set VERSION=0.1
- set MPV_TEST=mpv
+ set VERSION=0.4
+ set MPV_TEST=mpv --help
  set ZENITY_TEST=zenity --help
  set printf=echo
 
@@ -34,7 +34,7 @@ rem #    https://osspack32.googlecode.com/files/zenity.exe
  :checking_zenity
 
    %ZENITY_TEST%
-   if %ERRORLEVEL%==0 goto mpv_play_file
+   if %ERRORLEVEL%==0 goto mpv_check_input
    cls
    %printf%.
    %printf% - Error: zenity.exe not found! -
@@ -47,8 +47,24 @@ rem #    https://osspack32.googlecode.com/files/zenity.exe
    pause > nul
    exit
 
+ :mpv_check_input
+
+   cls
+   %printf%.
+   %printf% mpv-z initialized
+   if "%1" EQU "" (goto mpv_play_file) else (goto mpv_play_input)
+
+ :mpv_play_input
+
+   cls
+   %printf%.
+   %printf% mpv-z initialized
+   %printf%. 
+   %PLAYER% %1
+   exit
+
  :mpv_play_file  
-   
+
    cls
    %printf%.
    %printf% mpv-z initialized
